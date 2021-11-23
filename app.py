662d38c48cca3e391fa993bf4096ce3de5fc93c2
ِYousuf <yousufmehraban@gmail.com>
2021-11-22 17:10:15 -0800

    initial commit

--- app.py
+++ app.py
@@ -1,56 +1,56 @@
-from flask import Flask, request, render_template, flash, jsonify, redirect
-from surveys import surveys
+from flask import Flask, request, render_template, flash, jsonify, redirect, session
+from surveys import satisfaction_survey as survey
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'no secret'
 
-responses = []
-
+answer_key = 'answer'
 
 @app.route('/')
 def homepage():
-    survey_title = surveys['satisfaction'].title
-    survey_instruction = surveys['satisfaction'].instructions
-    for val in request.args:
-        responses.append(val)
-    print(responses)    
+    return render_template('homepage.html', survey = survey)
 
-    return render_template('homepage.html', title = survey_title, instruction = survey_instruction, redirect = redirect)
-
-@app.route('/question/1')
+@app.route('/question/1', methods=['POST', 'GET'])
 def show_first_question():
-    question1 = surveys['satisfaction'].questions[0].question
-    answer = surveys['satisfaction'].questions[0].choices
-    yes, no = answer
-    return render_template('question1.html', question1 = question1, yes = yes, no = no)
+    session[answer_key] = []
+    return render_template('question1.html', survey = survey)
 
-@app.route('/question/2')
+@app.route('/question/2', methods=['POST', 'GET'])
 def show_second_question():
-    question2 = surveys['satisfaction'].questions[1].question
-    answer = surveys['satisfaction'].questions[1].choices
-    yes, no = answer
-    for val in request.args:
-        responses.append(val)
-    return render_template('question2.html', question2 = question2, yes = yes, no = no)
+    answers = session[answer_key]
+    answers.append(request.form.get('answer'))
+    session[answer_key] = answers  
+    if (request.form.get('answer') is None):
+        return redirect('/question/1')
+    return render_template('question2.html', survey = survey)
 
-@app.route('/question/3')
+@app.route('/question/3', methods=['POST', 'GET'])
 def show_third_question():
-    question3 = surveys['satisfaction'].questions[2].question
-    answer = surveys['satisfaction'].questions[2].choices
-    yes, no = answer
-    for val in request.args:
-        responses.append(val)
-    return render_template('question3.html', question3 = question3, yes = yes, no = no)
+    answers = session[answer_key]
+    answers.append(request.form.get('answer'))
+    session[answer_key] = answers
+    if (request.form.get('answer') is None):
+        return redirect('/question/2')
+    return render_template('question3.html', survey = survey)
 
-@app.route('/question/4')
+@app.route('/question/4', methods=['POST', 'GET'])
 def show_fourth_question():
-    question4 = surveys['satisfaction'].questions[3].question
-    answer = surveys['satisfaction'].questions[3].choices
-    yes, no = answer
-    for val in request.args:
-        responses.append(val)
-    flash('Thanks for taking the survey with us')
-    return render_template('question4.html', question4 = question4, yes = yes, no = no)
+    answers = session[answer_key]
+    answers.append(request.form.get('answer'))
+    session[answer_key] = answers
+    if (request.form.get('answer') is None):
+        return redirect('/question/3')
+    return render_template('question4.html', survey = survey)
 
 
+@app.route('/thanks', methods=['POST', 'GET'])
+def thank_user():
+    answers = session[answer_key]
+    answers.append(request.form.get('answer'))
+    session[answer_key] = answers
+    if (request.form.get('answer') is None):
+        return redirect('/question/4')
+    print(session[answer_key])
+    flash('survey sucessfully submitted! ')
+    return render_template('thanks.html', survey = survey)
